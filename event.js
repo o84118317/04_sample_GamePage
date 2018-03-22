03b5e1b386036dcfb77e2669ce33959179900dbb
Chang hsi <o84118317@gmail.com>
2018-03-22 22:39:24 +0800

    add some alert prompt and comfirm

--- event.js
+++ event.js
@@ -1,1 +1,12 @@
-alert ('Yo');
+alert ('Hello' + ', Welcome to my page');
+
+let answer = prompt ('what\'s yout age?');
+if (answer < 18) {
+	if (confirm ('Sure you want to check this out?') ){
+		alert ('Okay, then welcome!');
+	} else {
+		alert ('See you');
+	}
+} else {
+	alert ('Welcome');
+}
